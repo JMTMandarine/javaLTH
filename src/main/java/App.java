a2f16016911e2e7a90a9484c5ec8d04a7a417b45
taehyeong <leeth4115@naver.com>
2018-09-04 20:21:25 +0900

    배열을 이용하여 정보 저장

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,33 +3,38 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        String name;
-        String email;
-        String password;
+        String[] name=new String[100] ;
+        String[] email=new String[100];
+        String[] password=new String[100];
         String answer;
+        int index=0;
         
         
         while(true) {
             
             System.out.println("이름을 입력해주세요 ㅎㅎ");
-            name=in.nextLine();
+            name[index]=in.nextLine();
             
             System.out.println("이메일을 입력해주세요 ㅎㅎ");
-            email=in.nextLine();
+            email[index]=in.nextLine();
             
             System.out.println("비밀번호을 입력해주세요 ㅎㅎ");
-            password=in.nextLine();
+            password[index]=in.nextLine();
             
-            System.out.printf("%s %s %s\n",name,email,password);
+            index++;
             
             System.out.println("계속입력하시겠습니까? (Y/n)");
             answer=in.nextLine();
-            
-            
-            
+   
             if(answer.toLowerCase().equals("n"))
-                break;
+                break;            
+        }
+        for(int i=0;i<index;i++) {
             
+            System.out.printf("%s %s %s\n"
+                            ,name[i]
+                            ,email[i]
+                            ,password[i]);
         }
         
         

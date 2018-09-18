976e3edc2c26934bf9a1aa124d901ab89faf76a0
taehyeong <leeth4115@naver.com>
2018-09-18 20:38:12 +0900

    배열을 이용하여 반복 및 정보저장

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,21 +3,31 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
+        String[] name=new String[5];
+        String[] email=new String[5];
+        String[] password=new String[5];
         
+        int index=0;
         while(true) {
             System.out.println("이름을 입력해주세용");
-            String name=in.nextLine();
+            name[index]=in.nextLine();
             System.out.println("이메일을 입력해주세용");
-            String email=in.nextLine();
+            email[index]=in.nextLine();
             System.out.println("비밀번호를 입력해주세용");
-            String password=in.nextLine();
-            System.out.printf("%s, %s, %s\n",name,email,password);
+            password[index]=in.nextLine();
+            
+            index++;
             
             System.out.println("계속하시겠습니까? (Y/n)");
             String answer=in.nextLine();
             if(answer.equals("n"))
                 break;
         }
+        
+        for(int i=0; i<index; i++) {
+            System.out.printf("%s, %s, %s\n",name[i],email[i],password[i]);
+        }
+        in.close();
     }
 }
 

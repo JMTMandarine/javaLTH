72e341f0828cd7e9a7d118f955bb0d975688c0f5
taehyeong <leeth4115@naver.com>
2018-09-18 20:49:03 +0900

    메서드로 코드 분리

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,24 @@
 import java.util.Scanner;
 
+
+
 public class App {
+    static Scanner in=new Scanner(System.in);
+    static String[] name=new String[5];
+    static String[] email=new String[5];
+    static String[] password=new String[5];
+    static int index=0;
+    
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        String[] name=new String[5];
-        String[] email=new String[5];
-        String[] password=new String[5];
+        inputMember();
         
-        int index=0;
+        printMember();
+
+        in.close();
+    }
+
+    static void inputMember() {
+        
         while(true) {
             System.out.println("이름을 입력해주세용");
             name[index]=in.nextLine();
@@ -15,19 +26,20 @@ public class App {
             email[index]=in.nextLine();
             System.out.println("비밀번호를 입력해주세용");
             password[index]=in.nextLine();
-            
+
             index++;
-            
+
             System.out.println("계속하시겠습니까? (Y/n)");
             String answer=in.nextLine();
             if(answer.equals("n"))
                 break;
         }
-        
+    }
+
+    static void printMember() {
         for(int i=0; i<index; i++) {
             System.out.printf("%s, %s, %s\n",name[i],email[i],password[i]);
         }
-        in.close();
     }
 }
 

3de7e2fe5478eccac5b6704ff34b3f4eb86d679a
taehyeong <leeth4115@naver.com>
2018-09-19 21:07:31 +0900

    메뉴 별도 분리 클래스

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -31,15 +31,56 @@ public class App {
     
     
     public static void main(String[] args) {
-        
-        
-        inputMember();
-        
-        printMember();
-
-        in.close();
+        while(true) {
+            String menu=promptMenu();
+            if(menu.equals("1")) {
+                serviceStudentMenu();
+            }else if(menu.equals("0")) {
+                System.err.println("시스템을 종료합니다.");
+                break;
+            }
+            
+            
+        }
     }
-
+    private static void serviceStudentMenu() {
+        while(true) {
+            System.out.println("<학생관리 시작(list/add/quit)>");
+            String command=in.nextLine();
+            if(command.equals("list")) {
+                printMember();
+            }else if(command.equals("add")) {
+                inputMember();
+            }else if(command.equals("quit")) {
+                break;
+            }else {
+                System.out.println("해당 메뉴가 존재하지 않습니다.");
+            }
+        }
+    }
+    
+    private static String promptMenu() {
+        System.out.println("[메뉴]");
+        System.out.println("1.학생관리");
+        System.out.println("2.강사관리");
+        System.out.println("3.매니저관리");
+        System.out.println("0.종료");
+        
+        while(true) {
+            System.out.println("메뉴번호 입력>");
+            String menu = in.nextLine();
+            
+            switch(menu){
+            case "1":
+            case "2":
+            case "3":
+            case "0":
+                return menu;
+            default:
+                System.out.println("해당하는 메뉴가 존재하지않습니다.");
+            }
+        }
+    }
     static void inputMember() {
         
         while(true) {

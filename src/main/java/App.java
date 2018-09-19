25d95a6dfbc874779da5c95db2c5b9b0757d8665
taehyeong <leeth4115@naver.com>
2018-09-19 20:42:45 +0900

    멤버 클래스 분리

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,38 @@
 import java.util.Scanner;
 
-
-
 public class App {
     static Scanner in=new Scanner(System.in);
-    static String[] name=new String[5];
-    static String[] email=new String[5];
-    static String[] password=new String[5];
     static int index=0;
+    static Member[] member=new Member[100];
+    
+    static class Member{
+        String name;
+        String email;
+        String password;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getEmail() {
+            return email;
+        }
+        public void setEmail(String email) {
+            this.email = email;
+        }
+        public String getPassword() {
+            return password;
+        }
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+    
     
     public static void main(String[] args) {
+        
+        
         inputMember();
         
         printMember();
@@ -20,15 +43,16 @@ public class App {
     static void inputMember() {
         
         while(true) {
+            Member m=new Member();
             System.out.println("이름을 입력해주세용");
-            name[index]=in.nextLine();
+            m.setName(in.nextLine());
             System.out.println("이메일을 입력해주세용");
-            email[index]=in.nextLine();
+            m.setEmail(in.nextLine());
             System.out.println("비밀번호를 입력해주세용");
-            password[index]=in.nextLine();
+            m.setPassword(in.nextLine());
 
-            index++;
-
+            
+            member[index++]=m;
             System.out.println("계속하시겠습니까? (Y/n)");
             String answer=in.nextLine();
             if(answer.equals("n"))
@@ -38,7 +62,10 @@ public class App {
 
     static void printMember() {
         for(int i=0; i<index; i++) {
-            System.out.printf("%s, %s, %s\n",name[i],email[i],password[i]);
+            System.out.printf("%s, %s, %s\n"
+                        ,member[i].getName()
+                        ,member[i].getEmail()
+                        ,member[i].getPassword());
         }
     }
 }
